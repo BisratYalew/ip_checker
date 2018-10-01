b7432f259a575d7fa2b8789b400cbf5fee74d610
Bisrat Yalew <bisratyalew10@gmail.com>
2018-10-01 11:52:55 -0700

    Added unittest for private ip checker function

--- test.py
+++ test.py
@@ -6,3 +6,4 @@ ip_obj = ip_checker('123.111.23.1')
 class IpCheckerTest(unittest.TestCase):
     def test(self):
         self.assertEqual(ip_obj.is_valid(), True)
+        self.assertEqual(ip_obj.is_private(), False)
