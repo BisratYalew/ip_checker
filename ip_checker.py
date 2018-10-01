69246528a228009c950763b64d5c9be77fd08c32
Bisrat Yalew <bisratyalew10@gmail.com>
2018-10-01 11:23:22 -0700

    Correct comment

--- ip_checker.py
+++ ip_checker.py
@@ -7,7 +7,7 @@ class ip_checker(object):
     def __init__(self, ipaddress):
         self.ip_address = ipaddress
 
-    ## This method return Boolean "True or False" by checking the given ip address
+    ## This method return Boolean "True or False" by checking wheter the given ip address is valid or not
     def is_valid(self):
         if not findall( "(?i)^(\d|\d\d|1[0-9][0-9]|2[0-9][0-5]).(\d|\d\d|1[0-9][0-9]|2[0-9][0-5]).(\d|\d\d|1[0-9][0-9]|2[0-9][0-5]).(\d|\d\d|1[0-9][0-9]|2[0-9][0-5])$" ,self.IP):
             return False
