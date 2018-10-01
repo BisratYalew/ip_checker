2282bcfc45fbe40feaaae62ec2f212cee44df808
Bisrat Yalew <bisratyalew10@gmail.com>
2018-10-01 11:30:55 -0700

    Fix if.else.if structure

--- ip_checker.py
+++ ip_checker.py
@@ -19,11 +19,11 @@ class ip_checker(object):
     def is_private(self):
         if findall( "(?i)^192.168.(\d|\d\d|1[0-9][0-9]|2[0-9][0-5]).(\d|\d\d|1[0-9][0-9]|2[0-9][0-5])$", self.ip_address ):
             return True
-        else if findall( '(?i)^127.\d{1,3}.\d{1,3}.\d{1,3}$',self.ip_address):
+        elif findall( '(?i)^127.\d{1,3}.\d{1,3}.\d{1,3}$',self.ip_address):
             return True
-        else if findall( '(?i)^10.(\d|\d\d|1[0-9][0-9]|2[0-9][0-5]).(\d|\d\d|1[0-9][0-9]|2[0-9][0-5]).(\d|\d\d|1[0-9][0-9]|2[0-9][0-5])$', self.IP):
+        elif findall( '(?i)^10.(\d|\d\d|1[0-9][0-9]|2[0-9][0-5]).(\d|\d\d|1[0-9][0-9]|2[0-9][0-5]).(\d|\d\d|1[0-9][0-9]|2[0-9][0-5])$', self.IP):
             return True
-        else if findall( '(?i)^172.(1[6-9]|2[0-9]|3[0-1]).(\d|\d\d|1[0-9][0-9]|2[0-9][0-5]).(\d|\d\d|1[0-9][0-9]|2[0-9][0-5])$', self.IP):
+        elif findall( '(?i)^172.(1[6-9]|2[0-9]|3[0-1]).(\d|\d\d|1[0-9][0-9]|2[0-9][0-5]).(\d|\d\d|1[0-9][0-9]|2[0-9][0-5])$', self.IP):
             return True
         else return False
 
