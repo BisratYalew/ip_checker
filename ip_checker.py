52fe2d0fe21f2b7d7ebe1ea00947639e6471f2f6
Bisrat Yalew <bisratyalew10@gmail.com>
2018-10-01 11:25:51 -0700

    Added method that checks if the given ipaddress is private or public

--- ip_checker.py
+++ ip_checker.py
@@ -12,4 +12,17 @@ class ip_checker(object):
         if not findall( "(?i)^(\d|\d\d|1[0-9][0-9]|2[0-9][0-5]).(\d|\d\d|1[0-9][0-9]|2[0-9][0-5]).(\d|\d\d|1[0-9][0-9]|2[0-9][0-5]).(\d|\d\d|1[0-9][0-9]|2[0-9][0-5])$" ,self.IP):
             return False
         else:
+            return True 
+
+    
+    ## This method checks whether the given ipaddress is private or public & returns True or False
+    def is_private(self):
+        if findall( "(?i)^192.168.(\d|\d\d|1[0-9][0-9]|2[0-9][0-5]).(\d|\d\d|1[0-9][0-9]|2[0-9][0-5])$", self.ip_address ):
             return True
+        else if findall( '(?i)^127.\d{1,3}.\d{1,3}.\d{1,3}$',self.ip_address):
+            return True
+        else if findall( '(?i)^10.(\d|\d\d|1[0-9][0-9]|2[0-9][0-5]).(\d|\d\d|1[0-9][0-9]|2[0-9][0-5]).(\d|\d\d|1[0-9][0-9]|2[0-9][0-5])$', self.IP):
+            return True
+        else if findall( '(?i)^172.(1[6-9]|2[0-9]|3[0-1]).(\d|\d\d|1[0-9][0-9]|2[0-9][0-5]).(\d|\d\d|1[0-9][0-9]|2[0-9][0-5])$', self.IP):
+            return True
+        else return False
